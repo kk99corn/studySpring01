ce9cd25d6b4c2af771751a6beacc4bd8372a7e0e
99 <hahahoho5915@gmail.com>
2022-04-20 20:55:49 +0900

    refactor: AppConfig 추가 및 의존관계 주입

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,16 +1,20 @@
 package hello.core.member;
 
-public class MemberServiceImpl implements MemberService{
+public class MemberServiceImpl implements MemberService {
 
-	private final MemberRepository m = new MemoryMemberRepository();	// OCP위반
+	private final MemberRepository memberRepository;
+
+	public MemberServiceImpl(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 
 	@Override
 	public void join(Member member) {
-		m.save(member);
+		memberRepository.save(member);
 	}
 
 	@Override
 	public Member findMember(Long memberId) {
-		return m.findById(memberId);
+		return memberRepository.findById(memberId);
 	}
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,14 +1,18 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
-	private final MemberRepository memberRepository = new MemoryMemberRepository();
-	private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+	private final MemberRepository memberRepository;
+	private final DiscountPolicy discountPolicy;    // 인터페이스만 의존하도록 수정
+
+	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+		this.memberRepository = memberRepository;
+		this.discountPolicy = discountPolicy;
+	}
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int price) {
