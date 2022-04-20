7addd0c1beb589ccafcfdd19067f0c9cacdc42aa
99 <hahahoho5915@gmail.com>
2022-04-20 21:51:13 +0900

    refactor: AppConfig 수정
    - 중복 코드 제거

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
@@ -9,18 +10,26 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 /*
-* AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성
-* */
+ * AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성
+ * */
 public class AppConfig {
 
 	public MemberService memberService() {
-		return new MemberServiceImpl(new MemoryMemberRepository());
+		return new MemberServiceImpl(memberRepository());
+	}
+
+	private MemoryMemberRepository memberRepository() {
+		return new MemoryMemberRepository();
 	}
 
 	public OrderService orderService() {
 		return new OrderServiceImpl(
-				new MemoryMemberRepository(),
-				new FixDiscountPolicy()
+				memberRepository(),
+				discountPolicy()
 		);
+	}
+
+	public DiscountPolicy discountPolicy() {
+		return new RateDiscountPolicy();
 	}
 }
